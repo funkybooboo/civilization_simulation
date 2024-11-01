68d4676c3eda9fcb4ac79c560defbe73c3d0690e
Nate Stott <nate.stott@pm.me>
2024-11-01 00:37:20 -0600

    add fix structure

--- app/src/main/java/org/echoes_of_eden/Main.java
+++ app/src/main/java/org/echoes_of_eden/Main.java
@@ -1,7 +1,12 @@
 package org.echoes_of_eden;
 
+import org.echoes_of_eden.Simulation.Simulation;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        // TODO generate many simulations
+        Simulation simulation = new Simulation();
+        simulation.run();
     }
 }
---
+++ app/src/main/java/org/echoes_of_eden/Simulation/Simulation.java
@@ -0,0 +1,7 @@
+package org.echoes_of_eden.Simulation;
+
+public class Simulation {
+    public void run() {
+
+    }
+}
