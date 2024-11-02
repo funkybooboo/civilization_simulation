3603376837a8a3da5798607221c763c01a7bea8a
Ann Marie Humble <A02324311@aggies.usu.edu>
2024-11-01 21:36:03 -0600

    added fields and methods for person class; added scheduler sub-class

--- src/Person.py
+++ src/Person.py
@@ -1,2 +1,45 @@
+from random import Random
+
+
 class Person:
-    pass
+    def __init__(self):
+        self.health: int = 100
+        self.hunger: int = 100  # when your hunger gets below 25, health starts going down; when it gets above 75, health starts going up
+        self.age = 0
+        self.home = None
+        self.spouse = None
+        self.scheduler = self.Scheduler()
+        # add a memory too, it's a list of tuples
+
+    def takeTurn(self, task):
+        tasks = []
+        # Check hunger and other stuff to see if you need to do a task
+
+        # Add all tasks you need to do to the scheduler
+
+        # Send the task off to the scheduler
+        self.scheduler.addTask(task)
+
+    def move():
+        # Remember things as they move
+        pass
+
+    class Scheduler:
+        def __init__(self):
+            self.tasks = []
+
+        def addTask(self, task):
+            # add task to queue using scheduling algorithm
+            pass
+
+# task is an abstract class; scheduler uses its fields
+# when a task is on the front of the queue, [scheduler?] calls task.getLocation() and if the person isn't there, then task.moveTo(), and if the person is there, then task.execute()
+# when a task is done, scheduler pops the task off the queue and calls task.execute([reference to that person], [reference to the environment])
+    # task.execute() will update the person and environment as necessary, depending on what task it is
+
+# SCHEDULER
+# check if there's a task currently running
+# get its priority
+# check the task scheduled next on the queue
+# see if you should swap the two tasks
+# execute the task that you're doing
