bc4f2994a028ada7630eedc082b067d0a918ae85
Ann Marie Humble <A02324311@aggies.usu.edu>
2024-11-01 19:40:27 -0600

    added a veeeeeeery rudimentary simulation file

---
+++ src/simulation.py
@@ -0,0 +1,12 @@
+class Simulation:
+    def __init__(self):
+        self.map = [] # an ascii map of the simulation
+        self.people = [] # a list of all people in the simulation
+        self.locations = {} # a dictionary of where all entities are on the map
+
+    def run(self):
+        # This is the main loop of the simulation
+        # For every person in people:
+        # Ask that person to decide what they're going to do
+        
+        pass
