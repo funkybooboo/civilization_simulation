fc87e46f6e34c681133c63504a26a5c89387a8b5
Ann Marie Humble <A02324311@aggies.usu.edu>
2024-11-01 21:39:25 -0600

    made comments prettier, added todos

--- src/Person.py
+++ src/Person.py
@@ -9,33 +9,36 @@ class Person:
         self.home = None
         self.spouse = None
         self.scheduler = self.Scheduler()
-        # add a memory too, it's a list of tuples
+        # TODO: add a memory; it's a list of tuples
 
     def takeTurn(self, task):
         tasks = []
-        # Check hunger and other stuff to see if you need to do a task
-
-        # Add all tasks you need to do to the scheduler
-
-        # Send the task off to the scheduler
-        self.scheduler.addTask(task)
+        # TODO: Check hunger and other stuff to see if you need to do a task, and add those tasks to the tasks list
+        for task in tasks:
+            self.scheduler.addTask(task)
 
     def move():
-        # Remember things as they move
+        # TODO: Move around and remember things as you move
         pass
 
     class Scheduler:
         def __init__(self):
+            # TODO: maybe change how tasks is being stored?
             self.tasks = []
 
         def addTask(self, task):
-            # add task to queue using scheduling algorithm
+            # TODO: add task to the queue using a scheduling algorithm
             pass
 
+# ======== NOTES ON HOW THIS SHOULD WORK ==========
+
 # task is an abstract class; scheduler uses its fields
-# when a task is on the front of the queue, [scheduler?] calls task.getLocation() and if the person isn't there, then task.moveTo(), and if the person is there, then task.execute()
-# when a task is done, scheduler pops the task off the queue and calls task.execute([reference to that person], [reference to the environment])
-    # task.execute() will update the person and environment as necessary, depending on what task it is
+# when a task is on the front of the queue, [scheduler?] calls task.getLocation() 
+    # and if the person isn't there, then task.moveTo(), 
+    # and if the person is there, then task.execute()
+# when a task is done, scheduler pops the task off the queue 
+    # and calls task.execute([reference to that person], [reference to the environment])
+# task.execute() will update the person and environment as necessary, depending on what task it is
 
 # SCHEDULER
 # check if there's a task currently running
