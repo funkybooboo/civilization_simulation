ded3026289021472e7d952a059b820f021dd3be6
Nate Stott <nate.stott@pm.me>
2024-11-07 08:43:41 -0700

    make memory feilds private

--- src/simulation/people/person/memory.py
+++ src/simulation/people/person/memory.py
@@ -5,33 +5,33 @@ from src.simulation.grid.location import Location
 
 class Memory:
     def __init__(self) -> None:
-        self.barns: Set[Location] = set()
-        self.construction_barns: Set[Location] = set()
-        self.farms: Set[Location] = set()
-        self.construction_farms: Set[Location] = set()
-        self.mines: Set[Location] = set()
-        self.construction_mines: Set[Location] = set()
-        self.homes: Set[Location] = set()
-        self.construction_homes: Set[Location] = set()
-        self.trees: Set[Location] = set()
-        self.empties: Set[Location] = set()
-        self.people: Set[Location] = set()
-        self.items: List[str] = list(vars(self).keys())
+        self._barns: Set[Location] = set()
+        self._construction_barns: Set[Location] = set()
+        self._farms: Set[Location] = set()
+        self._construction_farms: Set[Location] = set()
+        self._mines: Set[Location] = set()
+        self._construction_mines: Set[Location] = set()
+        self._homes: Set[Location] = set()
+        self._construction_homes: Set[Location] = set()
+        self._trees: Set[Location] = set()
+        self._empties: Set[Location] = set()
+        self._people: Set[Location] = set()
+        self._items: List[str] = list(vars(self).keys())
 
     def dont_know_where_anything_is(self) -> bool:
-        return not (self.barns or self.farms or self.homes or self.mines)
+        return not (self._barns or self._farms or self._homes or self._mines)
 
     def combine(self, other: "Memory") -> None:
-        for item in self.items:
+        for item in self._items:
             for location in getattr(other, item):
                 self.add(item, location)
 
     def add(self, what: str, where: Location) -> None:
         if what is None or where is None:
             return
-        if what not in self.items:
+        if what not in self._items:
             return
-        for item in self.items:
+        for item in self._items:
             if item != what:
                 self.__remove(item, where)
         getattr(self, what).add(where)
@@ -39,7 +39,7 @@ class Memory:
     def __remove(self, what: str, where: Location) -> None:
         if what is None or where is None:
             return
-        if what not in self.items:
+        if what not in self._items:
             return
         if where in getattr(self, what):
             getattr(self, what).remove(where)
@@ -47,16 +47,16 @@ class Memory:
     def __repr__(self) -> str:
         return str(
             {
-                "barns": self.barns,
-                "construction_barns": self.construction_barns,
-                "farms": self.farms,
-                "construction_farms": self.construction_farms,
-                "mines": self.mines,
-                "construction_mines": self.construction_mines,
-                "homes": self.homes,
-                "construction_homes": self.construction_homes,
-                "trees": self.trees,
-                "empties": self.empties,
-                "people": self.people,
+                "barns": self._barns,
+                "construction_barns": self._construction_barns,
+                "farms": self._farms,
+                "construction_farms": self._construction_farms,
+                "mines": self._mines,
+                "construction_mines": self._construction_mines,
+                "homes": self._homes,
+                "construction_homes": self._construction_homes,
+                "trees": self._trees,
+                "empties": self._empties,
+                "people": self._people,
             }
         )
