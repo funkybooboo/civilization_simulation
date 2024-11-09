f4347e6ca8f9fa8f0c66b463fe3fcb6306910c8b
Ann Marie Humble <A02324311@aggies.usu.edu>
2024-11-09 15:29:55 -0700

    expanded explore to explore further

--- src/simulation/people/person/scheduler/task/explore.py
+++ src/simulation/people/person/scheduler/task/explore.py
@@ -9,11 +9,15 @@ from src.simulation.simulation import Simulation
 class Explore(Task):
     def __init__(self, simulation: Simulation, person: Person) -> None:
         super().__init__(simulation, person, 5)
+        self._max_how_far: int = 5
+        self._how_far: int = 0
 
     @override
     def execute(self) -> None:
+        self._how_far += 1
         self._person.explore()
-        self._finished()
+        if self._how_far >= self._max_how_far:
+            self._finished()
 
     @override
     def _clean_up_task(self) -> None:
