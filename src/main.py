e80cdf3ec684812fc2753c37286949eaed740675
Nate Stott <nate.stott@pm.me>
2024-11-20 07:47:50 -0700

    add a timer in main so we can see how long it takes to run a simulation

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import time
 from src.logger import logger, setup_logger
 from src.settings import environment, settings
 from src.simulation.simulation import Simulation
@@ -13,13 +14,25 @@ def main() -> None:
 
     for i in range(max_simulations):
         logger.info(f"Running simulation {i + 1}")
-        simulation: Simulation = Simulation()
-        visualizer: Visualizer = simulation.run()
-        visualizer.display_town_slide_show()
-        visualizer.display_simulation_stats()
 
-        logger.info(f"Simulation {i + 1} completed")
+        # Start timing the simulation
+        start_time = time.time()
 
+        try:
+            simulation: Simulation = Simulation()
+            visualizer: Visualizer = simulation.run()
+            visualizer.display_town_slide_show()
+            visualizer.display_simulation_stats()
+
+        except Exception as e:
+            # Log the exception if something goes wrong
+            logger.error(f"An error occurred during simulation {i + 1}: {e}")
+
+        finally:
+            # End timing the simulation and log the duration
+            end_time = time.time()
+            simulation_duration = end_time - start_time
+            logger.info(f"Simulation {i + 1} completed in {simulation_duration:.2f} seconds")
 
 if __name__ == "__main__":
     main()
